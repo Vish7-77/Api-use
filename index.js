3072a5e182b1fd4ab26b749c1f90b8937fe5f053
vishal <Vishalgehlot334@gmail.com>
2023-03-16 16:48:35 +0530

    ok

---
+++ index.js
@@ -0,0 +1,194 @@
+
+
+//two steps in every program or any system 
+
+
+//1 -> memory allocation
+//2 -> Execution context 
+
+
+
+
+
+//stack memory and Heap memory
+// Global and Local
+
+
+//Shallow and Deep Copy
+//We have to know about the Data Type
+   //  {Premitive and Reference Data Type}
+   
+   
+   
+   // in JS this  is the Example of Pre. Data Type 
+
+   //let a = 10
+   // let b=a
+
+
+// in JS this  is the Example of Ref. Data Type 
+     // let x ={name:'std1', Class:'Btech' }
+     // let m = x
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// in javascript the Execution will be done line by line
+
+
+// alert('hello')
+
+
+//settimeout is an Asyn Function which takes a callback
+
+
+// function printThis(){
+//     console.log('try this')
+// }
+
+
+
+//function is a callback function
+// setTimeout(printThis,3000)
+
+//to overcome the call back hell we use promises
+
+
+
+// const a = new Promise(
+// (resolve,reject)=>{
+
+// //a Lower case
+// //A Uppercase case
+
+
+// if('A'=='A'){
+//     resolve('this is working')
+
+// }
+
+// else{
+//     reject('its not working')
+// }
+// }
+// )
+
+
+
+//it gets resolved or it will be in reject form
+//resolve shows the respose 
+//reject to Show the error
+
+
+
+//max used in APIs Fetching the api that time we use promises
+
+
+
+//API :- Server weather We get response or the error
+
+
+//fetch
+
+// a.then((res)=>console.log(res)).catch((err)=>console.log(err))
+
+
+// fetch().then((res)=>console.log(res)).catch((err)=>console.log(err))
+
+
+
+
+
+
+
+
+
+// console.log('hello')
+
+
+
+//-------- this is the example for Premitive Data Type
+
+// let x =10
+
+// //making Deep Copy 
+// let y= x
+// console.log(x)
+// console.log(y)
+
+// x=20
+
+// console.log(x)
+// console.log(y)
+
+
+
+
+//------------Reference Data Type in JavaScript 
+
+// let a ={name1:'James', class1:'2nd' }
+// let b =a
+
+
+// console.log(a)
+// console.log(b)
+
+// a.name1="rahul"
+
+// console.log(a)
+// console.log(b)
+
+
+
+//Always Points - 
+
+ // a:{} have a memory allocation of   memory/Storage  = 101010110 
+ // b =101010110
+
+ // refering the memory address
+
+
+//  Deep copy in Object 
+// let newObj = {cont:'india' , population:'140cr' }
+// let x = {...newObj}
+
+
+
+
+// console.log(x,newObj)
+
+
+// newObj.cont='qatar'
+// x.cont='US'
+// x.population='7cr'
+// console.log(x,newObj)
+
+
+// Upper All examples shows the memory Allocation and about the data types
+
+
+
+
+//integer , double, float, char, boolean
+
+
+
+
+//objects arrays graphs queues , stacks, trees 
+// const newObj = { namee1:'student1', class:'2nd' }
+// const NewObj2= {namee1:'student2', class:'3rd' }
+
+// https://catfact.ninja/fact
+
+
